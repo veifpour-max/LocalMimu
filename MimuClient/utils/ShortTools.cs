b659a2be8a83a2f3156e5703f4092c5c3c37719a
itsveiliss <sashas2811@gmail.com>
2026-06-09 21:42:28 +0300

    UI: Added smart time formatter (TimeHelper) for future Avalonia interface

--- MimuClient/utils/ShortTools.cs
+++ MimuClient/utils/ShortTools.cs
@@ -1,4 +1,5 @@
 namespace LocalMimu.Models;
+
 public static class shTools
 {
     public static bool check(string value)
@@ -9,5 +10,42 @@ public static class shTools
         }
         return true;
     }
+
+public static string FormatTime(DateTime time)
+    {
+        // тяжелее кода не писал, очень тяжело реально, ошибаешься как на первом дне. я долбаеб конченный вот откуда я мог заболеть да еще и так хреново
+        var timingDiff = DateTime.Now - time;
+
+        if (timingDiff.TotalSeconds >= 0 && timingDiff.TotalSeconds < 60)
+        {
+            return "Только что";
+        }
+        if (time.Date == DateTime.Now.Date)
+        {
+            return time.ToString("HH:mm");
+        }
+        if (time.Date == DateTime.Now.AddDays(-1).Date)
+        {
+            return "Вчера";
+        }
+        else
+        {
+           return time.ToString("dd.MM.yy");  
+        }
+        // я чувствую будто у меня -88 iq, даже применять в program.cs не хочу нахуй так жить блять
+    }
 }
 
+    
+    
+
+    
+
+
+
+
+
+
+
+
+    
