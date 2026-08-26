3087c39a1a1b3d11a10f2f48f34d5f65e3b8596b
itsveiliss <sashas2811@gmail.com>
2026-08-26 22:47:29 +0300

    Fixed bug that was spawning config file in the run-on directory

--- MimuServer/utils/ServerConfig.cs
+++ MimuServer/utils/ServerConfig.cs
@@ -8,12 +8,12 @@ public class ServerConfig
     public string MinioEndpoint { get; set; } = "ip:port";
     public string MinioUser { get; set; } = "your_minio_user";
     public string MinioPass { get; set; } = "your_password";
-    public string BucketName {get; set;} = "name_of_your_bucket";
+    public string BucketName { get; set; } = "name_of_your_bucket";
 }
 
 public static class ServerConfigLoader
 {
-    private static readonly string ConfigPath = "serversettings.json";
+    private static readonly string ConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "serversettings.json");
 
     public static ServerConfig Load()
     {
