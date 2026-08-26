2f79e49d1f57c837b0f0add7d97725344ebb7ada
itsveiliss <sashas2811@gmail.com>
2026-08-26 23:58:25 +0300

    Almost there...

--- MimuServer/lib/storages_group/DbConfig.cs
+++ MimuServer/lib/storages_group/DbConfig.cs
@@ -6,5 +6,5 @@ namespace LocalMimu.Repositories;
 public static class DbConfig
 {
     public static string ConnectionString =>
-    $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "localmimu.db")}";
+        $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "localmimu.db")}";
 }
